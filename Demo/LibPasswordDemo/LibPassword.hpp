4b96fefb18205b87bf7dcc0110b5407945640660
Zhang Zhixiao <131665599+JuRuoqwq@users.noreply.github.com>
2023-07-11 16:21:44 +0800

    项目演示文件

---
+++ Demo/LibPasswordDemo/LibPassword.hpp
@@ -0,0 +1,17 @@
+﻿/*
+* LibPassword API
+* Copyright (c) Storm Lab
+* 
+* This library can get password and
+* DO NOT print any thing or print some '*' 
+* 
+* This project is open source by Apache License 2.0
+* You can read it on <https://apache.org/licenses/>
+*/
+
+//定义宏
+#define NO_OUTPUT 0
+#define OUTPUT_DATA 1
+
+//获取输入，通过type判断是否输出
+extern char* getPassword(int,char);
---
+++ Demo/LibPasswordDemo/LibPasswordDemo.cpp
@@ -0,0 +1,26 @@
+﻿/*
+* LibPassword Project
+* Copyright (c) Storm Lab
+*
+* This library can get password and
+* DO NOT print any thing or print some '*'
+*
+* This project is open source by Apache License 2.0
+* You can read it on <https://apache.org/licenses/>
+*/
+
+#include <iostream>
+
+#include "LibPassword.hpp"
+
+#pragma comment(lib,"LibPassword.lib")
+
+int main(int argc, char* argv[])
+{
+	std::cout << "LibPassword Demo" << std::endl;
+	std::cout << "Password >> ";
+	getPassword(NO_OUTPUT, '\0');
+	getPassword(OUTPUT_DATA, '\0');
+	getPassword(OUTPUT_DATA, '@');
+	return 0;
+}
