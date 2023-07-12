f97a436f480932977d3307d31418796e0397302b
Zhang Zhixiao <131665599+JuRuoqwq@users.noreply.github.com>
2023-07-12 08:55:42 +0800

    增加了一些bug

--- src/LibPassword/LibPassword.cpp
+++ src/LibPassword/LibPassword.cpp
@@ -10,11 +10,12 @@
 */
 
 #include <conio.h>
+#include <Windows.h>
 
 #include "framework.h"
 #include "LibPassword.hpp"
 
-DLLEXPORT char* getPassword(int type,char outputChar)
+DLLEXPORT char* getPassword(int type, char outputChar)
 {
 	char output = '*'; //要输出的字符串，如果outputChar为''，则为'*'
 	char ch = 0; //要获取的字符
@@ -25,12 +26,6 @@ DLLEXPORT char* getPassword(int type,char outputChar)
 		if (_kbhit()) {
 			//如果按下键盘
 			ch = _getch();
-			if (ch == 13) {
-				//Enter
-				_putch('\n');
-				result[i] = '\0';
-				break;
-			}
 			if (type == OUTPUT_DATA) {
 				//有输出
 				if (outputChar != '\0') {
@@ -38,6 +33,21 @@ DLLEXPORT char* getPassword(int type,char outputChar)
 				}
 				_putch(output);
 			}
+			if (ch == 13 || ch == 9) {
+				//Enter or Tab
+				_putch('\n');
+				result[i] = '\0';
+				break;
+			}
+			//else if (ch == 8) {
+                                                                //这段代码有bug...
+				//退格
+			//	i--; i--; //让光标移动到前两格
+			//	system("cls");
+			//	for (int j = 0; j < i; j++) {
+			//		_putch(output);
+			//	}
+			//}
 			result[i] = ch;
 			i++;
 		}
